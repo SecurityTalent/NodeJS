f15c4e9933792cabc7678137e8fad0a8027a0280
SecurityTalent <thebrandbox101@gmail.com>
2026-06-28 16:22:38 +0600

    10 Creating a Simple Web Server

--- 10/index.js
+++ 10/index.js
@@ -1,13 +1,21 @@
-const fs = require("fs");
-const http = require("http");
+const fs = require('fs')
+const http = require('http')
+
 
 const server = http.createServer((req, res) => {
-    console.log(req);
-  res.end("Hello World");
+  console.log(req);
+  res.end('Hello form the server');
 });
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening on port 8000");
-});
+server.listen(8080, '127.0.0.1', () => {
+  console.log('Listening form request on port 8080')
+})
+
+
+
+
+
+
+
 
 
