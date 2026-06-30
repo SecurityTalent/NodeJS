29c45ea2ed8e886d32ac8dd0216876bd4fb3b50b
SecurityTalent <thebrandbox101@gmail.com>
2026-06-30 12:56:04 +0600

    14 HTML Templating Filling the Templates

--- 14/index.js
+++ 14/index.js
@@ -2,18 +2,24 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+
+
 const data = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+//! SERVER
 const server = http.createServer((req, res) => {
-
     const pathName = req.url;
 
+    //! overview page
     if (pathName === '/' || pathName === '/overview') {
         res.end("This is overview");
+
+    //! product page
     } else if (pathName === '/product') {
         res.end("This is product");
 
+    //! API
     } else if (pathName === '/api') {
 
         // fs.readFile(`${__dirname}/data.json`, 'utf-8', (err, data) => {
@@ -27,12 +33,11 @@ const server = http.createServer((req, res) => {
         // })
 
 
-
+    //! Not fount page 404
     } else {
         res.writeHead(404, {
             'content-type': 'text/html',
             'My-Header': 'Hello World'
-
         })
         res.end("<h1>Page not found</h1>");
     }
